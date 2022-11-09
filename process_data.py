4a15a2d054675e74bf8ef2a0dbd1bdfb5d3c5449
Kunn <yfeng55@jhu.edu>
2022-11-09 18:02:06 -0500

    update files

--- process_data.py
+++ process_data.py
@@ -7,17 +7,17 @@ from utils import read_fastq
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--input_file', type=str, default="data/SRR6756023.fastq", help='input fastq file')
-parser.add_argument('--mix_file', type=str, default="data/SRR6756025.fastq,data/SRR6756028.fastq", help='files to be mixed, separated by ","')
+parser.add_argument('--mix_files', type=str, default="data/SRR6756025.fastq,data/SRR6756028.fastq", help='files to be mixed, separated by ","')
 parser.add_argument('--output_file', type=str, default="output.fastq", help='output file')
 parser.add_argument('--mix_percentage', type=float, default=0.1, help='contamination level')
-parser.add_argument('--number_of_reads', type=int, default=1000000, help='total number of reads')
+parser.add_argument('--number_of_reads', type=int, default=1000, help='total number of reads')
 parser.add_argument('--mix_partition', type=int, default=10, help='whether to mix continues segments, default: False')
 
 args = parser.parse_args()
 
 
 def prepare_mix():
-    mix_filenames = args.mix_file.split(",")
+    mix_filenames = args.mix_files.split(",")
     number_read_per_file = int(args.number_of_reads * args.mix_percentage / len(mix_filenames))
 
     all_reads = []
